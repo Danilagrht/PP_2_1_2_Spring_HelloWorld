f3499418c5b11f5e8740021d43e9ed98b8ca2227
danila smyshlyaev <d.smyshlyaev@kodland.team>
2024-01-15 23:48:09 +0300

    cat class added, equality check added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean_1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorldBean_2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean_1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean_2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(helloWorldBean_1.equals(helloWorldBean_2));
+        System.out.println(catBean_1.equals(catBean_2));
     }
 }
